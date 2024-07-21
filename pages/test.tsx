6dab2f7ae9292e9e1f2f0134e85035be0815c2e4
Noor Ahmad Haral <noorahmadharal@gmail.com>
2024-07-22 00:04:43 +0500

    fixed

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,19 +3,19 @@
 // import Header from '../components/Header';
 // import Footer from '../components/footer';
 
-// const HomePage: React.FC = () => {
-//   return (
-//     <>
-//       <Header />
-//       <main className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-//         <ImageComparison 
-//           beforeImage="/black-logo.png.jpg"
-//           afterImage="/white-logo.png.jpg"
-//         />
-//       </main>
-//       <Footer />
-//     </>
-//   );
-// };
+const HomePage: React.FC = () => {
+  return (
+    <>
+      {/* <Header />
+      <main className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+        <ImageComparison 
+          beforeImage="/black-logo.png.jpg"
+          afterImage="/white-logo.png.jpg"
+        />
+      </main>
+      <Footer /> */}
+    </>
+  );
+};
 
-// export default HomePage;
+export default HomePage;
